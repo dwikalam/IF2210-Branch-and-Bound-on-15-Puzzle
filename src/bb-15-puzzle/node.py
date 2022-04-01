0678df8065a277a53078912394049b5f47506413
dwikalam <dwiikalam@gmail.com>
2022-04-02 06:54:00 +0700

    Add node class
    Node class is created for storing information about each node in 15-puzzle's space state tree,
    such as its respective configuration (as puzzle) and cost

---
+++ src/bb-15-puzzle/node.py
@@ -0,0 +1,59 @@
+import numpy as np
+from copy import copy
+
+class node:
+    def __init__(self, initializedNode = None):
+        self.__matrix = np.array([[0, 0, 0, 0],
+                                 [0, 0, 0, 0],
+                                 [0, 0, 0, 0],
+                                 [0, 0, 0, 0]])
+
+        self.__blankPos = { "row" : 0, 
+                            "col" : 0 }
+        self.__f_val = 0
+        self.__g_val = 0
+    
+    @classmethod
+    def move(cls, parentNode, moveDirection):
+        childNode = copy(parentNode)
+        childNode.__swapBlank(moveDirection)
+
+        return cls(childNode)
+
+    def __swapBlank(self, moveDirection):
+        blankRow = self.__blankPos["row"]
+        blankCol = self.__blankPos["col"]
+        rowToSwap = blankRow
+        colToSwap = blankCol
+
+        if (moveDirection == "RIGHT"):
+            colToSwap += 1
+
+        elif (moveDirection == "LEFT"):
+            colToSwap -= 1
+
+        elif (moveDirection == "DOWN"):
+            rowToSwap += 1
+
+        elif (moveDirection == "UP"):
+            rowToSwap -= 1
+
+        blankCellVal = self.__matrix[blankRow][blankCol]
+
+        self.__matrix[blankRow][blankCol] = self.__matrix[rowToSwap][colToSwap]
+        self.__matrix[rowToSwap][colToSwap] = blankCellVal
+
+        self.__blankPos["row"] = rowToSwap
+        self.__blankPos["col"] = colToSwap
+
+    def set_f_value(self, val):
+        self.__f_val = val
+    
+    def get_f_value(self):
+        return self.__f_val
+
+    def set_g_value(self, val):
+        self.__g_val = val
+
+    def get_g_value(self):
+        return self.__g_val
